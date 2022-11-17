456b49797f528de70ad09c692c5f7518a65347c0
sheikhsadi9494 <sadis6343@gmail.com>
2022-11-17 13:27:54 +0600

    delete opration

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
-// dbUesr3
-// mF4XxWELLZnVvDjo
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.cj5acxc.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cj5acxc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        const database = client.db('Car_Doc');
+        const servicesCollection = database.collection('services');
+        const ordersCollection = database.collection('orders')
+        
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor =  servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.send(service);
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result);
+        });
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if(req.query.email){
+                query = {email: req.query.email};
+            };
+            const cursor = ordersCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
+        app.delete('orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const order = await ordersCollection.deleteOne(query);
+            res.send(order);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Car Doctor Server Running');
